20867acc1e54ea394df28509ec953f854f60d1ef
streissen <streissen@gmail.com>
2017-04-30 17:30:54 +0800

    paper structure finished

---
+++ data/fft_use.m
@@ -0,0 +1,37 @@
+data = load('3.mat');
+amount = data.num(:, 3)';
+Fs = 1440;
+T = 1 /Fs;
+L = length(amount);
+t = (0:L-1) * T;
+
+Y = fft(amount);
+P2 = abs(Y);
+P1 = P2(1:L/200+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs*(0:(L/200))/L;
+plot(f,P1)
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+for index = 1:length(amount)
+    if abs(Y(index)) < 0.3e6
+        Y(index) = 0;
+    end
+end
+
+figure;
+exp_amount = abs(ifft(Y));
+day = 22;
+plot(t(day * 1440 + 1:(day + 1) * 1440),exp_amount(day * 1440 + 1:(day + 1) * 1440))
+title('ifft')
+
+figure;
+day = 22;
+plot(t(day * 1440 + 1:(day + 1) * 1440),amount(day * 1440 + 1:(day + 1) * 1440) - exp_amount(day * 1440 + 1:(day + 1) * 1440))
+title('delta')
+figure;
+day = 22;
+plot(t(day * 1440 + 1:(day + 1) * 1440),amount(day * 1440 + 1:(day + 1) * 1440))
+title('origin')
---
+++ reference/paper.md
@@ -0,0 +1,1 @@
+[基于小波变换的信号检测](http://read.pudn.com/downloads148/ebook/639832/%E5%B0%8F%E6%B3%A2%E5%88%86%E6%9E%90/%E5%B0%8F%E6%B3%A2%E5%88%86%E6%9E%90%E7%AA%81%E5%8F%98%E7%82%B9%E7%A0%94%E7%A9%B6.pdf)
